d96f68e3b6da5299c564e6e4ce3ead3dfd62f50a
Pietrell <piotrplotast2006@gmail.com>
2024-02-24 16:34:39 +0100

    Created and styled CtaButton component

---
+++ fylo-dark-theme-landing-page/src/CtaButton.tsx
@@ -0,0 +1,7 @@
+export default function CtaButton() {
+  return (
+    <button className="h-12 w-60 mt-8 text-textColor bg-gradient-to-r from-btnGradient1 to-btnGradient2 text-md font-bold rounded-[9999px]">
+      Get Started
+    </button>
+  );
+}

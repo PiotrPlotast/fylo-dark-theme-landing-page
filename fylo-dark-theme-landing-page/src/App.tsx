b65cae5ea012f7cb3aa3e39cb03ef117ba6b602c
Pietrell <piotrplotast2006@gmail.com>
2024-02-25 20:19:13 +0100

    added rest of components

--- fylo-dark-theme-landing-page/src/App.tsx
+++ fylo-dark-theme-landing-page/src/App.tsx
@@ -3,7 +3,12 @@ import Navbar from "./Navbar";
 import Hero from "./Hero";
 import Features from "./Features";
 import HowItWorks from "./HowItWorks";
-
+import Testimonials from "./Testimonials";
+import CtaCard from "./CtaCard";
+import Logo from "./Logo";
+import ContactUs from "./ContactUs";
+import FooterLinks from "./FooterLinks";
+import FooterSocials from "./FooterSocials";
 function App() {
   return (
     <div className="bg-bgColor">
@@ -14,8 +19,17 @@ function App() {
         <Hero />
         <Features />
         <HowItWorks />
+        <Testimonials />
+        <CtaCard />
       </main>
-      <footer></footer>
+      <footer className=" bg-footerBgColor">
+        <div className="pt-[15.1875rem] w-24 ml-10">
+          <Logo />
+        </div>
+        <ContactUs />
+        <FooterLinks />
+        <FooterSocials />
+      </footer>
     </div>
   );
 }

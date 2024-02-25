93dca557876926f28c197402c33ce13f7c329540
Pietrell <piotrplotast2006@gmail.com>
2024-02-25 18:23:10 +0100

    styled testimonial card

---
+++ fylo-dark-theme-landing-page/src/TestimonialCard.tsx
@@ -0,0 +1,28 @@
+export default function TestimonialCard({
+  message,
+  name,
+  avatar,
+  occupation,
+}: {
+  message: string;
+  name: string;
+  avatar: string;
+  occupation: string;
+}) {
+  return (
+    <div className="bg-cardBgColor px-5 pt-6 pb-5 rounded">
+      <p className="font-secondary text-textColor text-xs leading-[1.125rem]">
+        {message}
+      </p>
+      <div className="mt-4 flex">
+        <img src={avatar} alt="" className="w-6 h-6 rounded-full mr-2" />
+        <div className="">
+          <h3 className="font-secondary text-textColor text-xs font-bold">
+            {name}
+          </h3>
+          <p className="font-secondary text-textColor text-xxs">{occupation}</p>
+        </div>
+      </div>
+    </div>
+  );
+}

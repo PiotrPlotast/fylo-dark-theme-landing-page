3b5cfdb0ca3703bd4b88ddbc39b117d432a63ab5
Pietrell <piotrplotast2006@gmail.com>
2024-02-25 19:44:22 +0100

    added testimonials section

---
+++ fylo-dark-theme-landing-page/src/Testimonials.tsx
@@ -0,0 +1,37 @@
+import TestimonialCard from "./TestimonialCard";
+import avatar1 from "./assets/images/profile-1.jpg";
+import avatar2 from "./assets/images/profile-2.jpg";
+import avatar3 from "./assets/images/profile-3.jpg";
+
+export default function Testimonials() {
+  return (
+    <section className="mt-40 bg-bg-quotes bg-left-top max-w-lg mx-auto">
+      <ul className="flex flex-col gap-6 mx-12">
+        <li>
+          <TestimonialCard
+            message="Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has become a well-oiled collaboration machine."
+            name="Satish Patel"
+            avatar={avatar1}
+            occupation="Founder & CEO, Huddle"
+          />
+        </li>
+        <li>
+          <TestimonialCard
+            message="Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has become a well-oiled collaboration machine."
+            name="Bruce McKenzie"
+            avatar={avatar2}
+            occupation="Founder & CEO, Huddle"
+          />
+        </li>
+        <li>
+          <TestimonialCard
+            message="Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has become a well-oiled collaboration machine."
+            name="Iva Boyd"
+            avatar={avatar3}
+            occupation="Founder & CEO, Huddle"
+          />
+        </li>
+      </ul>
+    </section>
+  );
+}

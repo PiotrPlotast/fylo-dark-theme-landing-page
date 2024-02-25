535f0f452c502e2f77683e22f330f8c6d2b1583c
Pietrell <piotrplotast2006@gmail.com>
2024-02-25 20:19:54 +0100

    created and styled FooterLinks and Footer Socials

---
+++ fylo-dark-theme-landing-page/src/FooterLinks.tsx
@@ -0,0 +1,17 @@
+export default function FooterLinks() {
+  return (
+    <section className="mx-7 mt-14">
+      <ul className="mb-12 flex flex-col gap-4">
+        <li className="text-textColor font-secondary">About us</li>
+        <li className="text-textColor font-secondary">Jobs</li>
+        <li className="text-textColor font-secondary">Press</li>
+        <li className="text-textColor font-secondary">Blog</li>
+      </ul>
+      <ul className="flex flex-col gap-4">
+        <li className="text-textColor font-secondary">Contact us</li>
+        <li className="text-textColor font-secondary">Terms</li>
+        <li className="text-textColor font-secondary">Privacy</li>
+      </ul>
+    </section>
+  );
+}
---
+++ fylo-dark-theme-landing-page/src/FooterSocials.tsx
@@ -0,0 +1,62 @@
+export default function FooterSocials() {
+  return (
+    <section className="flex justify-center gap-4 mt-14 pb-16">
+      <a href="#" className="border border-textColor rounded-full p-1">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="icon icon-tabler icon-tabler-brand-facebook-filled"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          strokeWidth="2"
+          stroke="white"
+          fill="none"
+          strokeLinejoin="round"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path
+            d="M18 2a1 1 0 0 1 .993 .883l.007 .117v4a1 1 0 0 1 -.883 .993l-.117 .007h-3v1h3a1 1 0 0 1 .991 1.131l-.02 .112l-1 4a1 1 0 0 1 -.858 .75l-.113 .007h-2v6a1 1 0 0 1 -.883 .993l-.117 .007h-4a1 1 0 0 1 -.993 -.883l-.007 -.117v-6h-2a1 1 0 0 1 -.993 -.883l-.007 -.117v-4a1 1 0 0 1 .883 -.993l.117 -.007h2v-1a6 6 0 0 1 5.775 -5.996l.225 -.004h3z"
+            strokeWidth="0"
+            fill="white"
+          />
+        </svg>
+      </a>
+      <a href="#" className="border border-textColor rounded-full p-1">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="icon icon-tabler icon-tabler-brand-twitter"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          strokeWidth="2"
+          stroke="white"
+          fill="none"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M22 4.01c-1 .49 -1.98 .689 -3 .99c-1.121 -1.265 -2.783 -1.335 -4.38 -.737s-2.643 2.06 -2.62 3.737v1c-3.245 .083 -6.135 -1.395 -8 -4c0 0 -4.182 7.433 4 11c-1.872 1.247 -3.739 2.088 -6 2c3.308 1.803 6.913 2.423 10.034 1.517c3.58 -1.04 6.522 -3.723 7.651 -7.742a13.84 13.84 0 0 0 .497 -3.753c0 -.249 1.51 -2.772 1.818 -4.013z" />
+        </svg>
+      </a>
+      <a href="#" className="border border-textColor rounded-full p-1">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="icon icon-tabler icon-tabler-brand-instagram"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          strokeWidth="2"
+          stroke="white"
+          fill="none"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M4 4m0 4a4 4 0 0 1 4 -4h8a4 4 0 0 1 4 4v8a4 4 0 0 1 -4 4h-8a4 4 0 0 1 -4 -4z" />
+          <path d="M12 12m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
+          <path d="M16.5 7.5l0 .01" />
+        </svg>
+      </a>
+    </section>
+  );
+}

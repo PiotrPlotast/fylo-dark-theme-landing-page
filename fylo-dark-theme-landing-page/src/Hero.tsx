099325fccc0d5491770f6e5014de0516329ce6c3
Pietrell <piotrplotast2006@gmail.com>
2024-02-28 19:45:28 +0100

    updated font and spacing for bigger screens

--- fylo-dark-theme-landing-page/src/Hero.tsx
+++ fylo-dark-theme-landing-page/src/Hero.tsx
@@ -4,7 +4,7 @@ export default function Hero() {
   return (
     <section className="flex flex-col items-center">
       <HeroImg />
-      <h1 className="text-textColor text-xl font-bold font-main text-center mx-7 mt-8 mb-4 max-w-[30ch] lg:text-xxl">
+      <h1 className="text-textColor text-xl font-bold font-main text-center mx-7 mt-8 mb-4 max-w-[30ch] lg:text-xxl lg:mb-8">
         All your files in one secure location, accessible anywhere.
       </h1>
       <p className="text-textColor text-md font-main text-center mx-10 max-w-[50ch] lg:text-[1.25rem]">

853240e9a9bc04b7827a2eaf2ae62d16450a26ef
Pietrell <piotrplotast2006@gmail.com>
2024-02-29 20:46:27 +0100

    added required

--- fylo-dark-theme-landing-page/src/CtaCard.tsx
+++ fylo-dark-theme-landing-page/src/CtaCard.tsx
@@ -1,5 +1,8 @@
 import CtaButton2 from "./CtaButton2";
 export default function CtaCard() {
+  const handleSubmit = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+  };
   return (
     <section className="flex flex-col items-center bg-accessCardBgColor mx-5 pb-9  rounded translate-y-40 md:max-w-4xl md:mx-auto">
       <h2 className="text-textColor font-main font-bold text-lg mt-10 md:text-[2rem]">
@@ -11,13 +14,14 @@ export default function CtaCard() {
         help you.
       </p>
       <form
-        action=""
+        onSubmit={handleSubmit}
         className="flex flex-col lg:flex-row lg:items-center lg:gap-7"
       >
         <input
+          required
           type="text"
           placeholder="email@example.com"
-          className=" rounded-full h-12 w-[17.5rem] pl-8 mb-6 text-accessCardInputColor placeholder:text-accessCardInputColor font-secondary text-sm lg:mb-0 lg:w-[30rem]"
+          className=" rounded-full h-12 w-[17.5rem] pl-8 mb-6 text-accessCardInputColor placeholder:text-accessCardInputColor font-secondary text-sm lg:mb-0 lg:w-[30rem] required:border required:border-[red]"
         />
         <CtaButton2 />
       </form>

56ab4e6d0f1f5ea8226b3b3b17e97f2cff2b2e66
Pietrell <piotrplotast2006@gmail.com>
2024-02-29 17:48:49 +0100

    added classes for big screens

--- fylo-dark-theme-landing-page/src/CtaButton2.tsx
+++ fylo-dark-theme-landing-page/src/CtaButton2.tsx
@@ -1,6 +1,6 @@
 export default function CtaButton2() {
   return (
-    <button className="h-12 w-[17.5rem] mt-6 text-textColor bg-gradient-to-r from-btnGradient1 to-btnGradient2 text-md font-bold rounded-[9999px] font-main">
+    <button className="h-12 w-[17.5rem] text-textColor bg-gradient-to-r from-btnGradient1 to-btnGradient2 text-md font-bold rounded-[9999px] font-main lg:max-w-52">
       Get Started For Free
     </button>
   );
